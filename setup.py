f5401a1c7f7f059332d57f35370dbb6cec346483
polymonster <alexandercdixon@gmail.com>
2019-12-08 16:31:26 +0000

    - add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jsn-polymonster",
+    version="0.0.1",
+    author="Alex Dixon",
+    author_email="alexandercdixon@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/polymonster/jsn",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
