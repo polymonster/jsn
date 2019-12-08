a15734873221eb6252fe35c02aa3773f88ea55c7
Alex Dixon <alexandercdixon@gmail.com>
2019-12-08 18:10:57 +0000

    Update setup.py

--- pypi/setup.py
+++ pypi/setup.py
@@ -18,6 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    package_dir={'': 'jsn'}
+    python_requires='>=3.6'
 )
